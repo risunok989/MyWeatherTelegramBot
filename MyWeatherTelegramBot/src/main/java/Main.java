7f16b8daaae351ba02164b5f3a3f7ab8a71c1993
risunok989 <risunok989@gmail.com>
2022-04-19 19:56:23 +0300

    sdfdf

---
+++ MyWeatherTelegramBot/src/main/java/Main.java
@@ -0,0 +1,54 @@
+import WatherParser.OpenWeatherMapJsonParser;
+import org.json.simple.parser.ParseException;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import java.io.IOException;
+
+public class Main {
+    public static final String PATH_TO_PROPERTIES = "src/main/resources/config.properties";
+
+    public static void main(String[] args) throws TelegramApiException, IOException, ParseException, InterruptedException {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+           }
+
+    public static String getBotTokenProp() {
+        try {
+
+            FileInputStream fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            Properties prop = new Properties();
+            prop.load(fileInputStream);
+            System.out.println("tokenBot");
+            return prop.getProperty("tokenBot");
+        } catch (IOException e) {
+            e.fillInStackTrace().toString();
+        }
+        return null;
+    }
+
+    public static String genUserNameBot() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            Properties prop = new Properties();
+            prop.load(fileInputStream);
+            System.out.println("userNameBot");
+            return prop.getProperty("nameBot");
+        } catch (IOException e) {
+            e.fillInStackTrace().toString();
+        }
+        return null;
+    }
+}
---
+++ MyWeatherTelegramBot/src/main/java/Util/Prop.java
@@ -0,0 +1,13 @@
+package Util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Prop {
+
+
+    FileInputStream fileInputStream;
+
+
+}
